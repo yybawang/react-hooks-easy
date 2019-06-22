88b6fe3a1f8aa6d08739c1fccfda3bad2b9a355d
yybawang <512511253@qq.com>
2019-06-22 21:49:38 +0800

    module導出

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./useModule');
+/**
+ * @name react-hooks-easy
+ * @author yybawang@gmail.com
+ */
+
+import {useBoolean} from './useBoolean/useBoolean'
+import {useNumber} from './useNumber/useNumber'
+import {useObject, useMap} from './useObject/useObject'
+import {useArray, useList} from "./useArray/useArray";
+import {useString} from "./useString/useString";
+
+export default {useBoolean, useNumber, useObject, useMap, useArray, useList, useString};
