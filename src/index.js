ecde6fe684d053fe1bee837f2b23901b0f4816dc
yybawang <512511253@qq.com>
2019-06-22 21:44:10 +0800

    文檔編寫

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-/**
- * @name react-hooks-easy
- * @author yybawang@gmail.com
- */
-
-import {useBoolean} from './useBoolean/useBoolean'
-import {useNumber} from './useNumber/useNumber'
-import {useObject, useMap} from './useObject/useObject'
-import {useArray, useList} from "./useArray/useArray";
-import {useString} from "./useString/useString";
-
-export {useBoolean, useNumber, useObject, useMap, useArray, useList, useString};
+module.exports = require('./useModule');
---
+++ src/useModule.js
@@ -0,0 +1,12 @@
+/**
+ * @name react-hooks-easy
+ * @author yybawang@gmail.com
+ */
+
+import {useBoolean} from './useBoolean/useBoolean'
+import {useNumber} from './useNumber/useNumber'
+import {useObject, useMap} from './useObject/useObject'
+import {useArray, useList} from "./useArray/useArray";
+import {useString} from "./useString/useString";
+
+export {useBoolean, useNumber, useObject, useMap, useArray, useList, useString};
